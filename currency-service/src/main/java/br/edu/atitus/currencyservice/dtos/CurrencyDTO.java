107cdc545f23ccbfd1303e14236f72cf57e7aa28
Pedro Henrique Franceschi <pedrohenriquekyre@gmail.com>
2026-04-17 21:51:52 -0300

    finalizando controller, agora podemos utilizar o navegador para testar os endpoints

--- currency-service/src/main/java/br/edu/atitus/currencyservice/dtos/CurrencyDTO.java
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/dtos/CurrencyDTO.java
@@ -1,7 +1,6 @@
 package br.edu.atitus.currencyservice.dtos;
 
 public record CurrencyDTO(
-        Long id,
         String sourceCurrency,
         String targetCurrency,
         Double conversionRate,
